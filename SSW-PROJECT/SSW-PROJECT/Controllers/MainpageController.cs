259c13c3c29d47defae3ba5abdb9c01078e51b84
Viktorbjornlund <viktor.bjornlund@gmail..com>
2015-02-19 15:54:18 +0100

    second commit

---
+++ SSW-PROJECT/SSW-PROJECT/Controllers/MainpageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSW_PROJECT.Controllers
+{
+    public class MainpageController : Controller
+    {
+        //
+        // GET: /Mainpage/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Searchpage()
+        {
+            return View();
+        }
+
+        public ActionResult Browsepage()
+        {
+            return View();
+        }
+
+        //public ActionResult searchFunction()
+        //{
+
+        //}
+    }
+}
