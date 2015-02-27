459230ba8a4e4abad50e26d33b42319a3f82d950
Knasfan <nilsolofsson@live.se>
2015-02-27 16:23:53 +0100

    Lagt till BL

---
+++ SSW-PROJECT/BL/Books.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BL
+{
+    public class Books
+    {
+        static Books()
+        {
+        }
+    }
+}
