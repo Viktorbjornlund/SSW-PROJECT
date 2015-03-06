003bf85d42d731938babdd12a00efcb2c4b3181f
Knasfan <nilsolofsson@live.se>
2015-03-06 15:29:23 +0100

    fix2

--- SSW-PROJECT/BL/User.cs
+++ SSW-PROJECT/BL/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BL
 {
-    class User
+    public class User
     {
         public string UserName { get; set; }
         public string Password { get; set; }
